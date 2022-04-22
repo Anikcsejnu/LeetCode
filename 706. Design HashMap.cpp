de2e72ce790e56a5717facc97e08632caa82fa95
Mohammad Atikur Rhaman <atikur.rhaman@enosisbd.com>
2022-04-22 11:16:40 +0600

    Solve 706. Design HashMap

---
+++ 706. Design HashMap.cpp
@@ -0,0 +1,19 @@
+class MyHashMap {
+public:
+    vector<int>hashMap;
+    MyHashMap() {
+        hashMap.resize(1e6+1, -1);    
+    }
+    
+    void put(int key, int value) {
+        hashMap[key] = value;
+    }
+    
+    int get(int key) {
+        return hashMap[key];
+    }
+    
+    void remove(int key) {
+        hashMap[key] = -1;
+    }
+};

b6a0191593ae73e4ed5570d5cd211c4a53eed178
anikcsejnu <anikcsejnu@gmail.com>
2022-06-21 23:42:20 +0600

    Solve '155. Min Stack'

---
+++ 155. Min Stack.cpp
@@ -0,0 +1,30 @@
+class MinStack {
+public:
+    stack<pair<int, int>> st;
+    int mini = INT_MAX;
+    
+    MinStack() {
+    }
+    
+    void push(int val) {
+        if (st.empty()){
+            mini = val;
+        }
+        else {
+            mini = min(val, st.top().second);
+        }
+        st.push({val, mini});
+    }
+    
+    void pop() {
+        st.pop();
+    }
+    
+    int top() {
+        return st.top().first;
+    }
+    
+    int getMin() {
+        return st.top().second;
+    }
+};

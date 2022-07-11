3322cab4191ce15e7e8d810661497e50e9c447eb
Anikcsejnu <anikraju110@gmail.com>
2022-07-11 18:34:20 +0600

    Add new approach for 338. Counting Bits

--- 338. Counting Bits.cpp
+++ 338. Counting Bits.cpp
@@ -1,4 +1,4 @@
-// Naive approach
+// O(nlog(n)) approach
 
 class Solution {
 public:
@@ -16,6 +16,21 @@ public:
             res.push_back(count);
         }
         
+        return res;
+    }
+};
+
+//O(n) approach
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> res(n + 1);
+        res[0] = 0;
+         
+        for(int i = 0;i <= n;i++) {
+            res[i] = res[i / 2] + i % 2;
+        }
         return res;
     }
 };

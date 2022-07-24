22ff866cc0444bd47b1b2688c5336e5308d658b7
anikcsejnu <anikraju110@gmail.com>
2022-07-24 23:02:20 +0600

    Add dfs implementation for '463. Island Perimeter'

--- 463. Island Perimeter.cpp
+++ 463. Island Perimeter.cpp
@@ -19,3 +19,34 @@ public:
         return parameter;
     }
 }; 
+
+// dfs implementation
+
+class Solution {
+public:
+    int dfs(vector<vector<int>>& grid, int r, int c) {
+        
+        if(r < 0 || c < 0 || r >= grid.size() 
+           || c >= grid[0].size() || grid[r][c] == 0) return 1;
+        
+        if(grid[r][c] == 2)return 0;
+        grid[r][c] = 2;
+        
+        return dfs(grid, r + 1, c) + dfs(grid, r - 1, c) 
+            + dfs(grid, r, c + 1) + dfs(grid, r, c-1); 
+    }
+    
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int ans = 0;
+        for(int i = 0;i < grid.size();i++) {
+            for(int j = 0;j < grid[0].size();j++) {
+                if (grid[i][j]) {
+                    ans += dfs(grid, i, j);
+                    break;
+                } 
+            }
+        } 
+        
+        return ans;
+    }
+};

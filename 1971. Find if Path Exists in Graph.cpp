a1c21355f54d957532af6501c6e3333a75a8cae7
anikcsejnu <anikraju110@gmail.com>
2022-07-27 22:56:36 +0600

    Solve '1971. Find if Path Exists in Graph' problem using DFS

--- 1971. Find if Path Exists in Graph.cpp
+++ 1971. Find if Path Exists in Graph.cpp
@@ -1,3 +1,4 @@
+// Solve using BFS
 class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
@@ -32,6 +33,42 @@ public:
                 }
             }
         }
+        return false;
+    }
+};
+
+// Solve using dfs
+
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        vector<vector<int>> graph(n);
+        
+        for(int i = 0;i < edges.size();i++) {
+            graph[edges[i][0]].push_back(edges[i][1]);
+            graph[edges[i][1]].push_back(edges[i][0]);
+        }
+        
+        vector<int> visited(n, 0);
+        
+        bool ans = dfs(graph, visited, source, destination);
+        return ans;
+    }
+    
+    bool dfs(vector<vector<int>>& graph, vector<int>& visited, int current, int end) {
+        if(visited[current])
+            return false;
+        
+        if(current ==  end)
+            return true;
+        
+        visited[current] = 1;
+        
+        for(int i = 0;i < graph[current].size();i++) {
+            if(dfs(graph, visited, graph[current][i], end))
+               return true;
+        }
+        
         return false;
     }
 };

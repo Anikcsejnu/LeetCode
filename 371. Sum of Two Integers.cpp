14bac06457643891d6445242c943821f2e0e40b3
Anikcsejnu <anikraju110@gmail.com>
2022-08-02 21:58:06 +0600

    Solve '371. Sum of Two Integers' with bit manupulation

--- 371. Sum of Two Integers.cpp
+++ 371. Sum of Two Integers.cpp
@@ -21,3 +21,35 @@ public:
         
     }
 };
+
+// Used bit manupulation
+
+class Solution {
+public:
+    int getSum(int a, int b) {
+        
+        int sum = 0, carry = 0, res = 0;
+        
+        for(int i = 0;i < 32;i++){
+            int d1 = a & 1, d2 = b & 1;
+            
+            if(d1 == 0 && d2 == 0) {
+                sum = carry == 0 ? 0 : 1;
+                carry = 0;
+            }
+            else if(d1 == 1 && d2 == 1) {
+                sum = carry == 0 ? 0 : 1;
+                carry = 1;
+            }
+            else {
+                sum = carry == 0 ? 1 : 0;
+            }
+            
+            res |= sum << i;
+            a = a >> 1;
+            b = b >> 1;
+        }
+        
+        return res;
+    }
+}; 

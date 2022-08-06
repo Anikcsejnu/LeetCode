073cc984a72b583d35a11cdfcd33fc1056938bea
anikcsejnu <anikraju110@gmail.com>
2022-08-06 23:35:30 +0600

    Add goland solution on '70. Climbing Stairs' problem

--- 70. Climbing Stairs.cpp
+++ 70. Climbing Stairs.cpp
@@ -13,3 +13,19 @@ public:
         return dp[n];
     }
 };
+
+
+// Go solution 
+
+func climbStairs(n int) int {
+    var dp [46]int
+    
+    dp[0] = 1
+    dp[1] = 1
+    
+    for i := 2; i <= n;i++ {
+        dp[i] = dp[i-1] + dp[i-2]
+    }
+    
+    return dp[n]
+}

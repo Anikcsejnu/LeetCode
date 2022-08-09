44bb5735b942c0868a3dc5103bae5632cc08255b
Anikcsejnu <anikraju110@gmail.com>
2022-08-09 16:28:42 +0600

    Solve 322 with bottom to top approach

--- 322. Coin Change.cpp
+++ 322. Coin Change.cpp
@@ -23,3 +23,27 @@ public:
         return ans == INT_MAX - 1? -1: ans;
     }
 };
+
+// Recursion Bottom to Top
+
+class Solution {
+public:
+    int dp[10000 + 1][12 + 1];
+    
+    int func(vector<int>& coins, int amount, int n) {
+        if(n == 0 || amount == 0) return amount == 0? 0 : INT_MAX - 1;
+        
+        if(dp[amount][n] != -1) return dp[amount][n];
+        
+        if (coins[n - 1] > amount) return dp[amount][n] = func(coins, amount, n - 1);
+        else return dp[amount][n] = min(func(coins, amount - coins[n - 1], n)  + 1, func(coins, amount, n - 1) + 0); 
+    }
+    
+    int coinChange(vector<int>& coins, int amount) {
+        memset(dp, -1, sizeof(dp));
+        int n = coins.size();
+        int ans = func(coins, amount, n);
+        
+        return ans == INT_MAX - 1? -1: ans;
+    }
+};

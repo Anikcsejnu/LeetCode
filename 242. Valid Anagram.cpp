82eecc51e10697f51c6b5c72c3df3365f4c03fa2
Anikcsejnu <anikraju110@gmail.com>
2022-08-10 22:44:48 +0600

    Add optimized solution to 242

--- 242. Valid Anagram.cpp
+++ 242. Valid Anagram.cpp
@@ -18,3 +18,25 @@ public:
         return true; 
     }
 };
+
+// optimized solution
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int mp[26] = {0};
+        
+        for(auto c: s) {
+            mp[c - 'a']++;
+        }
+        for(auto c: t) {
+            mp[c - 'a']--;
+        }
+        
+        for(int i = 0;i < 26;i++) {
+            if(mp[i] != 0) return false;
+        }
+        
+        return true;
+    }
+};

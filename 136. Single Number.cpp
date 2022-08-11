c3d9f6b3ea40bd634148ad6305da1d5c78a1f8cf
Anikcsejnu <anikraju110@gmail.com>
2022-08-11 11:40:17 +0600

    Solved '136. Single Number'

---
+++ 136. Single Number.cpp
@@ -0,0 +1,23 @@
+// Time: O(N)
+// Space: O(N)
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<int, int> mp;  
+        int ans = 0;
+        
+        for(int i = 0;i < nums.size();i++) {
+            mp[nums[i]]++;
+        }
+        
+        for(auto item: mp) {
+            if(item.second == 1) {
+                ans = item.first;
+                break;
+            }
+        }
+        
+        return ans;
+    }
+};

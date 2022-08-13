46626900c102fe290923ade966da91fbc8bf9a7d
Anikcsejnu <anikraju110@gmail.com>
2022-08-13 14:37:30 +0600

    Solve 11. Container With Most Water using Two Pointer Binary Search

--- 11. Container With Most Water.cpp
+++ 11. Container With Most Water.cpp
@@ -1,3 +1,5 @@
+// Naive solution, Time Complexity O(N^2)
+
 class Solution {
 public:
     int maxArea(vector<int>& height) {
@@ -11,3 +13,28 @@ public:
         return ans;
     }
 };
+
+// Using Two Pointer
+// Time Complexity O(N)
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int ans = 0, area = 0, n = height.size();
+        int start = 0, end = n - 1;
+        
+        while(start < end) {
+            area = min(height[start], height[end]) * (end - start);
+            ans = max(ans, area);
+            
+            if(height[start] > height[end]) {
+                end--;
+            }
+            else {
+                start++;
+            }
+        }
+        return ans;
+    }
+};
+

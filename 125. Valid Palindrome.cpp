5a70f94dd87670dddb8b035e631949ee3710ccae
anikcsejnu <anikraju110@gmail.com>
2022-08-17 22:10:11 +0600

    Solve using two pointer '125. Valid palindrome'

--- 125. Valid Palindrome.cpp
+++ 125. Valid Palindrome.cpp
@@ -16,3 +16,31 @@ public:
         return rev == alphaNumeric ? true : false; 
     }
 };
+
+// Solved using two pointer
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string alphaNumeric = "", rev = "";
+        int len = s.size();
+        
+        for(int i = 0;i < len;i++) {
+            if(isdigit(s[i]) || isalpha(s[i])) {
+                alphaNumeric += tolower(s[i]);  
+            }
+        }
+        
+        int left = 0, right = alphaNumeric.size() - 1;
+        while(left < right) {
+            if(alphaNumeric[left] == alphaNumeric[right]) {
+                left++;
+                right--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true; 
+    }
+};

3d093bc0852b94bc48d73359e3770aea67e16738
anikcsejnu <anikraju110@gmail.com>
2022-08-18 12:57:25 +0600

    Solve using counting sort '49. Group anagrams'

--- 49. Group Anagrams.cpp
+++ 49. Group Anagrams.cpp
@@ -19,3 +19,38 @@ public:
         return ans;
     }
 };
+
+// Use counting sort to solve 
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        vector<vector<string>> ans;
+        
+        for(auto &s: strs) {
+            string t = s;
+            t = countingSort(t);
+            mp[t].push_back(s);
+        }
+        
+        for(auto &it: mp) {
+            ans.push_back(it.second);
+        }
+        
+        return ans;
+    }
+    
+    string countingSort(string str) {
+        int mp[26] ={0};
+        string t = "";
+        
+        for(auto &s: str) {
+            mp[s - 'a']++;
+        }
+        
+        for(int i = 0;i < 26;i++) {
+            t += string(mp[i], i + 'a');    
+        }
+        return t;
+    }
+};

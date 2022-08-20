14e2c2149b221bbd5dc93dc72636b92a7655a614
anikcsejnu <anikraju110@gmail.com>
2022-08-20 22:38:41 +0600

    Solve '200. Number of Islands' using DFS

--- 200. Number of Islands.cpp
+++ 200. Number of Islands.cpp
@@ -1,3 +1,5 @@
+// Solve using DFS
+
 class Solution {
 public:
     void dfs(vector<vector<char>>& grid, int i, int j, int m, int n) {
@@ -22,6 +24,50 @@ public:
             }
         }
 
+        return ans;
+    }
+};
+
+// Solve using BFS
+
+class Solution {
+public:
+    Solution() {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+    }
+        
+    int numIslands(vector<vector<char>>& grid) {
+        int ans = 0, m = grid.size(), n = grid[0].size();
+        int dir[5] = {0, -1, 0, 1, 0};
+        
+        for(int i = 0;i < m;i++) {
+            for(int j = 0;j < n;j++) {
+                if(grid[i][j] == '1') {
+                    queue<pair<int, int>> q;
+                    
+                    q.push({i, j});
+                    ans++;
+                    grid[i][j] = '0';
+                    
+                    while(!q.empty()) {
+                        auto [x, y] = q.front();
+                        q.pop();
+                        
+                        for(int k = 0;k < 4;k++) {
+                            int newX = x + dir[k];
+                            int newY = y + dir[k + 1];
+                            
+                            if(newX >= 0 && newY >= 0 && newX < m && newY < n && grid[newX][newY] == '1') {
+                                grid[newX][newY] = '0';
+                                q.push({newX, newY});    
+                            }
+                            
+                        }
+                    }
+                }
+            }
+        }
         return ans;
     }
 };

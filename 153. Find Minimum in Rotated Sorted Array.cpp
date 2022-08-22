19049089803c30bcf9796485c911b14fd48deac5
anikcsejnu <anikraju110@gmail.com>
2022-08-22 23:31:55 +0600

    Solve using GO

--- 153. Find Minimum in Rotated Sorted Array.cpp
+++ 153. Find Minimum in Rotated Sorted Array.cpp
@@ -17,3 +17,20 @@ public:
         return nums[start];
     }
 };
+
+// Solve using go
+
+func findMin(nums []int) int {
+    start, end, mid := 0, len(nums) - 1, 0
+    
+    for start < end {
+        mid = (start + end) / 2
+        
+        if(nums[mid] < nums[end]) {
+            end = mid
+        } else {
+            start = mid + 1
+        }
+    }
+    return nums[start]
+}

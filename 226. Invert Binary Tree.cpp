985a9800351e5afaad8cc77eaf11d19958457855
Anikcsejnu <anikraju110@gmail.com>
2022-09-01 23:47:06 +0600

    Go solution for 226

--- 226. Invert Binary Tree.cpp
+++ 226. Invert Binary Tree.cpp
@@ -22,3 +22,17 @@ public:
         return root;
     }
 };
+
+// Go solution
+
+func invertTree(root *TreeNode) *TreeNode {
+    if root == nil {
+        return root
+    }   
+    
+    root.Left, root.Right = root.Right, root.Left
+    invertTree(root.Left)
+    invertTree(root.Right)
+    
+    return root
+}

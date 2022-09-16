7215a015a30e1df1d8666e2061e92b1600bfc0a0
Anikcsejnu <anikraju110@gmail.com>
2022-09-16 23:11:25 +0600

    Solve 112 with peak valley approach

--- 122. Best Time to Buy and Sell Stock II.cpp
+++ 122. Best Time to Buy and Sell Stock II.cpp
@@ -14,3 +14,31 @@ public:
         return profit;
     }
 };
+
+
+// Peak Valley approach
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int len = prices.size(), profit = 0;
+        int peak = INT_MAX;
+        int valley = peak;
+        
+        for(int i = 0;i < len;i++) {
+            if(prices[i] < peak) {
+                profit += peak - valley;
+                
+                peak = prices[i];
+                valley = peak;
+            }
+            else {
+                peak = prices[i];
+            }
+        }
+        
+        profit += peak - valley;
+        
+        return profit;
+    }
+};

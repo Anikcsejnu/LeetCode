bf485ac6e18c4bed8b73edd1657f2a2e5580e08d
Anikcsejnu <anikraju110@gmail.com>
2022-09-28 23:35:48 +0600

    Solve 66 with optimized way

--- 66. Plus One.cpp
+++ 66. Plus One.cpp
@@ -17,3 +17,31 @@ public:
         return digits;
     }
 };
+
+
+// Optimized Time - O(N), Space - O(1)
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int i = digits.size() - 1, carry = 1;
+        
+        while(i >= 0) {
+            if(digits[i] == 9 && carry == 1) {
+                digits[i] = 0;
+            }
+            else {
+                digits[i] = digits[i] + 1;  
+                carry = 0;
+                break;
+            }
+            i--;
+        }
+        
+        if(carry == 1) {
+            digits.insert(digits.begin(), 1);
+        }
+        
+        return digits;
+    }
+};

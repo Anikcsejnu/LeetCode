cb8cbc3040801aca05b9267cbb366d61e01b6a3a
Anikcsejnu <anikraju110@gmail.com>
2022-10-11 23:18:20 +0600

    Solve 33 again

--- 33. Search in Rotated Sorted Array.cpp
+++ 33. Search in Rotated Sorted Array.cpp
@@ -83,3 +83,57 @@ public:
         }
     }
 };
+
+// Updated the approach
+
+class Solution {
+private:
+    int getPivot(vector<int>& nums) {
+        int start = 0, end = nums.size() - 1;
+        
+        while(start < end) {
+            int mid = start + (end - start) / 2;
+            
+            if(nums[0] <= nums[mid]) {
+                start = mid + 1;
+            }
+            else {
+                end = mid;
+            }
+        }
+        return start;
+    }
+    
+    int binarySearch(vector<int>& nums, int start, int end, int target) {
+        
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            
+            if(nums[mid] < target) {
+                start = mid + 1;
+            }
+            else if(nums[mid] > target) {
+                end = mid - 1;
+            }
+            else {
+                return mid;
+            }
+        }
+        
+        return -1;
+    }
+    
+public:
+    int search(vector<int>& nums, int target) {
+        int len = nums.size();
+        int pivot = getPivot(nums);
+        
+        if((nums[pivot] <= target) && (target <= nums[len-1])) {
+            return binarySearch(nums, pivot, len - 1, target);
+        }
+        else {
+            return binarySearch(nums, 0, pivot - 1, target);
+        }
+        
+    }
+};

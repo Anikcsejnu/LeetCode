ad684dd82bc5355ead3a2e97211378bffef41510
Anikcsejnu <anikraju110@gmail.com>
2022-10-17 23:01:56 +0600

    Solve more optimize way 1832

--- 1832. Check if the Sentence Is Pangram.cpp
+++ 1832. Check if the Sentence Is Pangram.cpp
@@ -10,3 +10,20 @@ public:
         return st.size() == 26? true: false;
     }
 };
+
+// Optimized way
+// Bit manupulation
+
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        int occured = 0;
+        
+        for(int i = 0;i < sentence.size();i++) {
+            int curr = 1 << int(sentence[i] -'a');
+            occured = occured | curr;
+        }
+        
+        return occured == (1 << 26) - 1? true: false;
+    }
+};

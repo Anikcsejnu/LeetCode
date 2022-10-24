f7f8dd0b2f83146325c33aa403648285398dc166
Anikcsejnu <anikraju110@gmail.com>
2022-10-24 22:29:44 +0600

    Solved 567 again

--- 567. Permutation in String.cpp
+++ 567. Permutation in String.cpp
@@ -30,3 +30,40 @@ public:
     }
     
 };
+
+// Solved again
+
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        vector<int> fqs1(26, 0);
+        for(auto c: s1) fqs1[c - 'a']++;
+        
+        vector<int> fqs2(26, 0);
+        
+        int l = 0, r = 0;
+        
+        while(r < s2.size()) {
+            fqs2[s2[r] - 'a']++;
+            
+            if(r - l + 1  == s1.size()) {
+                if(fqs1 == fqs2) {
+                    return true;
+                }
+            }
+            
+            if(r - l + 1 < s1.size()) {
+                r++;
+            }
+            else {
+                fqs2[s2[l] - 'a']--;
+                l++;
+                r++;
+                
+            }
+        }
+        
+        return false;
+    }
+    
+};

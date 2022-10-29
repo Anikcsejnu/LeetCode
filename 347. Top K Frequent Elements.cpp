9861cf0c68e21b80801fd04b7768d1864c48c2fe
Anikcsejnu <anikraju110@gmail.com>
2022-10-29 13:09:36 +0600

    Solve 347 again

--- 347. Top K Frequent Elements.cpp
+++ 347. Top K Frequent Elements.cpp
@@ -22,3 +22,36 @@ public:
         
     }
 };
+
+
+// Solve again
+
+class Solution {
+public:    
+    static bool cmp(pair<int, int> a, pair<int, int> b) {
+        return a.second > b.second;
+    }
+    
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<pair<int, int>> vp;
+        map<int, int> mp;
+        vector<int> ans;
+        int len = nums.size();
+        
+        for(int i = 0;i < len;i++) {
+            mp[nums[i]]++;
+        }
+        
+        for(auto v: mp) {
+            vp.push_back(make_pair(v.first, v.second));
+        }
+        
+        sort(vp.begin(), vp.end(), cmp);
+        
+        for(int i = 0;i < k;i++) {
+            ans.push_back(vp[i].first);
+        }
+        
+        return ans;
+    }
+}; 

de440049a846c9e44424ad6f52af1b29e4d8848f
Anikcsejnu <anikraju110@gmail.com>
2022-11-15 20:02:17 +0600

    Solve leetcode 11 no prob

--- 11. Container With Most Water.cpp
+++ 11. Container With Most Water.cpp
@@ -38,3 +38,26 @@ public:
     }
 };
 
+// Solve at 15 Nove
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0, right = height.size() - 1;
+        int ans = INT_MIN;
+        
+        while(left < right) {
+            int minHeight = min(height[left], height[right]);
+            ans = max(ans, (minHeight * (right - left)));
+            
+            if(height[left] < height[right]) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+        return ans;
+    }
+};
+
